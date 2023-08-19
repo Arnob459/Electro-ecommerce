95d73d0095631021f2f346e0d402ffdd7d3ada19
Mahin zaman Arnob <arnobmahin007@gmail.com>
2023-08-19 12:20:38 +0600

    frontend

--- resources/views/frontend/frontend.blade.php
+++ resources/views/frontend/frontend.blade.php
@@ -535,28 +535,22 @@
 
     <div class="mb-4">
 
-        {{-- <div class="bg-img-hero" style="background-image: url( assets/img/1920X422/img2.jpg);"> --}}
             <div class="container overflow-hidden">
                 <div class="js-slick-carousel u-slick"
                     data-pagi-classes="text-center position-absolute right-0 bottom-0 left-0 u-slick__pagination u-slick__pagination--long justify-content-center mb-3 mb-md-4">
                     @foreach ($sliders as $slider)
-
                     <div class="js-slide">
                         <div class="row pt-7 py-md-0">
-
                             <div class="col-xl-7 col-6 d-flex align-items-end ml-auto ml-md-0"
                                 data-scs-animation-in="fadeInRight"
                                 data-scs-animation-delay="500">
-                                <img class="img-fluid ml-auto mr-10 mr-wd-auto" src="{{ $slider->picture }}" alt="Image Description">
+                                <img  src="{{ $slider->picture }}" alt="Image Description">
                             </div>
                         </div>
-                    </div>
                     @endforeach
-
+                    </div>
                 </div>
             </div>
-        {{-- </div> --}}
-
     </div>
 
     <!-- End Slider Section -->
